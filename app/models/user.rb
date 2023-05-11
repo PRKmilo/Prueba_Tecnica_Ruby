32923a0d3d47247575ea738ff4c3549239ed27da
malboro-eng <jpizarodrugiez@gmail.com>
2023-05-11 13:00:05 -0500

    Se agrega validacion de con la gema device ademas de los diferentes formularios de de login y de signup

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :email, uniqueness: { message: "Esa direccion de correo electronico ya a sido tomada" }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+  root 'main#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
