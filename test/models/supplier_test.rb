83f00d087ab7b30bdad2e54eb060a29aac83b7e5
malboro-eng <jpizarodrugiez@gmail.com>
2023-05-15 00:24:47 -0500

    Enviando el test de supplier para el modelo

--- test/models/supplier_test.rb
+++ test/models/supplier_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class SupplierTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "shoud not save a supplier withput nit" do
+     supplier = Supplier.new
+     assert_not supplier.save
+     assert supplier.errors.messages[:nit] == ["can't be blans"]
+   end
+
+   test "should not save without bank_id" do
+     supplier = Supplier.new
+     assert_not supplier.save
+     assert supplier.errors.messages[:bank_id] == ["can't be blank"]
+   end
 end
